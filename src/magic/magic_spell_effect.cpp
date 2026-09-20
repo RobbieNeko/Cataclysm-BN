b16cf1065060411d1b61c93babc7bab2c342e411
RobbieNeko <30732426+RobbieNeko@users.noreply.github.com>
2026-09-20 11:31:17 -0700

    Attempt manually formatting file
    autofix is angry at life and refusing to work

--- src/magic/magic_spell_effect.cpp
+++ src/magic/magic_spell_effect.cpp
@@ -107,7 +107,8 @@ static void build_line(
     std::set<tripoint_bub_ms>& result) {
     auto last_point = source;
     while (between_or_on(point_zero, delta, delta_perp, line.get())) {
-        // Ordered intentionally so that the first point to fail the test is still included in the affected area (i.e. it hits the wall)
+        // Ordered intentionally so that the first point to fail the test is still included
+        // in the affected area (i.e. it hits the wall)
         result.emplace(source + line.get());
         if (!test(source + line.get(), last_point)) { break; }
         last_point = source + line.get();
@@ -162,7 +163,7 @@ static auto in_spell_aoe(
         if ((here.impassable(pt) && !here.has_flag("THIN_OBSTACLE", pt))
             || here.obstructed_by_vehicle_rotation(pt, last_point)) {
             // We desire the spell to *hit* the wall, not stop short of it.
-            // Therefore, if the obstruction is the end of the line, then we do consider it to be in the AOE
+            // If the obstruction is the end of the line, then we still consider it in the AOE
             return pt == end;
         }
         last_point = pt;
@@ -207,7 +208,8 @@ static auto spell_effect_cone_range_override(
                     && (here.passable(tp) || here.has_flag("THIN_OBSTACLE", tp)))) {
                 targets.emplace(tp);
             } else {
-                // We want it to hit the wall, not stop short, so we still include this point but not any further
+                // We want it to hit the wall, not stop short, so we still include this point
+                // but not any further
                 targets.emplace(tp);
                 break;
             }
